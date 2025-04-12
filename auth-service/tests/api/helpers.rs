54ddafe28afd8de91bf04bb5c8836d2b22b08d40
Thompson Edolo <verygreenboi@live.com>
2025-04-12 12:20:08 +0100

    Add integration tests for root route in auth-service
    Introduced a test infrastructure for the auth-service, including the `TestApp` helper for managing a test-deployed application. Added an integration test to check that the root route returns a 200 status with the correct "text/html" content type. Updated dependencies to include `reqwest` for HTTP client support in tests.

---
+++ auth-service/tests/api/helpers.rs
@@ -0,0 +1,40 @@
+use auth_service::Application;
+
+pub struct TestApp {
+    pub address: String,
+    pub http_client: reqwest::Client,
+}
+
+const TEST_SERVER_HOST: &str = "127.0.0.1:0";
+
+
+impl TestApp {
+    pub async fn new() -> Self {
+        let app = Application::build(TEST_SERVER_HOST)
+            .await
+            .expect("Failed to build app");
+
+        let address = format!("http://{}", app.address.clone());
+
+        // Run the auth service in a separate async task
+        // to avoid blocking the main test thread.
+        #[allow(clippy::let_underscore_future)]
+        let _ = tokio::spawn(app.run());
+
+        let http_client = reqwest::Client::builder()
+            .timeout(std::time::Duration::from_secs(10))
+            .build().expect("Failed to build reqwest client");
+
+
+        // Create new `TestApp` instance and return it
+        Self { address, http_client }
+    }
+
+    pub async fn get_root(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+}
